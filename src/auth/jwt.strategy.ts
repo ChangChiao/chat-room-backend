f18897c8ad50db85f970d6a11959b3f15833f596
JoeCT_Chang <JoeCT.Chang@moxa.com>
2024-08-20 08:56:57 +0800

    fix: fix jwtStrategy

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,13 +1,14 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(private configService: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: 'yourSecretKey',
+      secretOrKey: configService.get<string>('JWT_SECRET'),
     });
   }
   async validate(payload: any) {
