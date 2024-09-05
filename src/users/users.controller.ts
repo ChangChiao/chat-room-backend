da725391a0e50db565064b11531430eba5304474
JoeCT_Chang <JoeCT.Chang@moxa.com>
2024-09-05 16:08:41 +0800

    feat(bess): add user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
-
+import { Controller, Get, Post, Delete, Param, Body } from '@nestjs/common';
+import { User } from 'src/schema/user.entity';
+import { UsersService } from './users.service';
 @Controller('users')
-export class UsersController {}
+export class UserController {
+  constructor(private readonly userService: UsersService) {}
+
+  @Get()
+  findAll(): Promise<User[]> {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number): Promise<User> {
+    return this.userService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() user: Partial<User>): Promise<User> {
+    return this.userService.create(user);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: number): Promise<void> {
+    return this.userService.remove(id);
+  }
+}
