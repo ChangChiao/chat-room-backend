242ec7404da52757226b82dece58e764a40703bb
JoeCT_Chang <JoeCT.Chang@moxa.com>
2024-09-22 11:21:56 +0800

    feat: add cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api/v1');
+
+  app.enableCors({
+    origin: 'http://localhost:5000',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
