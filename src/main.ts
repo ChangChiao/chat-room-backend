4327527522a92b94a3787f70b7b65e8e1c038caa
JoeCT_Chang <JoeCT.Chang@moxa.com>
2025-04-08 16:01:15 +0800

    feat: add methods for enable cors

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: 'http://localhost:5000',
-    methods: ['GET', 'POST'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
