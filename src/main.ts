2d751a1f5d857f987fbbce94c837cde95d28cd7a
JoeCT_Chang <JoeCT.Chang@moxa.com>
2025-04-09 10:05:07 +0800

    feat: add http exception filters

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
+import { HttpExceptionFilter } from './filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api/v1');
   app.use(cookieParser());
+  app.useGlobalFilters(new HttpExceptionFilter());
 
   app.enableCors({
     origin: 'http://localhost:5000',
